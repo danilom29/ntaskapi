39e218ee3ea048970f6f92d134be0992723b0e10
danilobarbosa <mdanilo.13@gmail.com>
2018-02-16 17:33:56 -0300

    capitulo 6

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,7 @@
 module.exports = app => {
   app.db.sequelize.sync({force:true}).done(() => {
     app.listen(app.get("port"), () => {
-      console.log(`NTASK API - porta `);
+      console.log(`NTASK API - porta ${app.get("port")}`);
     });
   });
 }
