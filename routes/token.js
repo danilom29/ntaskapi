8a3e53d01dd77bcdd50015b77a56e639f9c204ba
danilobarbosa <mdanilo.13@gmail.com>
2018-03-01 09:40:08 -0300

    conslusão capitulo 10

--- routes/token.js
+++ routes/token.js
@@ -3,6 +3,23 @@ const jwt = require("jwt-simple");
 module.exports = app => {
 	const cfg = app.libs.config;
 	const Users = app.db.models.Users; 
+	/**
+	* @api {post} /token Token autenticado
+	* @apiGroup Credencial
+	* @apiParam {String} email Email de Usuário
+	* @apiParam {String} password Senha de usuário
+	* @apiParamExample {json} Entrada
+	*	{
+	*		"email": "danilo@connor.net",
+	*		"password": "123456"
+	*	}
+	* @apiSuccess {String} token Token de usuário autenticado
+	* @apiSuccessExample {json} Sucesso
+	*	HTTP/1.1 200 OK
+	*	{"token": "xyz.zbc.123.hgf"}
+	* @apiErrorExample {json} Erro de autenticação
+	*	HTTP/1.1 401 Unauthorized
+	*/
 	app.post("/token", (req, res) => {
 		if(req.body.email && req.body.password){
 			const email = req.body.email;
